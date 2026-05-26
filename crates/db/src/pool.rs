2b387eb872d071becedf648e4a91448cfa19da3d
whoisasx <adil.business4064@gmail.com>
2026-05-26 16:17:41 +0530

    pool.rs file added

---
+++ crates/db/src/pool.rs
@@ -0,0 +1,36 @@
+use std::sync::OnceLock;
+
+use sqlx::{Pool, postgres::{PgPoolOptions, Postgres} , migrate::MigrateError};
+
+static DB_POOL: OnceLock<Pool<Postgres>> = OnceLock::new();
+
+pub async fn create_db_pool(database_url: &str) -> Result<Pool<Postgres>, sqlx::Error> {
+    PgPoolOptions::new()
+        .max_connections(20)
+        .connect(database_url)
+        .await
+}
+
+pub fn init_pool(pool: Pool<Postgres>) {
+    match DB_POOL.set(pool) {
+        Ok(()) => {}
+        Err(_) => panic!("db_pool already exists"),
+    }
+}
+
+pub fn pool() -> &'static Pool<Postgres> {
+    match DB_POOL.get() {
+        Some(pool) => pool,
+        None => panic!("db_pool is not initialized"),
+    }
+}
+
+pub fn poool() -> &'static Pool<Postgres> {
+    pool()
+}
+
+pub async fn run_migration() -> Result<(), MigrateError>{
+  sqlx::migrate!("./migrations")
+    .run(pool())
+    .await
+}
