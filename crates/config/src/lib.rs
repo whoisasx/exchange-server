5d23650a90add16df38d59089a30c7ecd38b44c3
whoisasx <adil.business4064@gmail.com>
2026-05-26 16:17:04 +0530

    added config

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -1,5 +1,19 @@
+use std::env;
 use dotenvy::dotenv;
 
-pub fn config_envs() -> Result<(), dotenvy::Error> {
-    dotenv().map(|_| ())
+#[derive(Debug)]
+pub struct Config{
+    pub database_url: String
 }
+
+impl Config{
+    pub fn init() -> Config {
+        dotenv().ok();
+        let database_url=env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+        Config { 
+            database_url
+        }
+    }
+}
+
