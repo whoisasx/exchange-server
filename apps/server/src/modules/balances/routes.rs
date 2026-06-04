b042bca16ef86315f6fc9fb0da457d68bdde2da2
whoisasx <adil.business4064@gmail.com>
2026-06-04 12:44:39 +0530

    added the config in balance/routes.rs

---
+++ apps/server/src/modules/balances/routes.rs
@@ -0,0 +1,14 @@
+use actix_web::{middleware::from_fn, web::{self, ServiceConfig}};
+
+use crate::{middlewares::auth, modules::balances::handlers};
+
+pub fn config_balance_routes(cfg: &mut ServiceConfig){
+  cfg.service(
+    web::scope("/balance")
+        .wrap(from_fn(auth::auth_middleware))
+        .service(handlers::add_balance)
+        .service(handlers::get_balance)
+        .service(handlers::get_available_balance)
+        .service(handlers::get_currency_balance)
+  );
+}
